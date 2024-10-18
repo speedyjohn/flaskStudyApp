ba653b396394586467c99d25e11dff500e15fc6d
speedyjohn <nachprog@gmail.com>
2024-10-18 16:53:54 +0500

    lesson CRUD complete

--- routes/Admin/AdminLessonRoutes.py
+++ routes/Admin/AdminLessonRoutes.py
@@ -6,45 +6,56 @@ from models import Lesson, db
 class AdminLessonRoutes:
     def __init__(self, bp: Blueprint):
         self.bp = bp
+        self.prefix = "/admin/lessons/"
 
-        self.bp.add_url_rule("/admin/lessons/", view_func=self.get_all)
-        self.bp.add_url_rule("/admin/lessons/<int:id>/", view_func=self.get_by_id)
-        self.bp.add_url_rule("/admin/lessons/create/", view_func=self.create, methods=["POST", "GET"])
-        self.bp.add_url_rule("/admin/lessons/edit/<int:id>", view_func=self.edit, methods=["PUT", "GET"])
+        self.bp.add_url_rule(f"{self.prefix}", view_func=self.get_all)
+        self.bp.add_url_rule(f"{self.prefix}<int:id>/", view_func=self.get)
+        self.bp.add_url_rule(f"{self.prefix}create/", view_func=self.create)
+        self.bp.add_url_rule(f"{self.prefix}insert/", view_func=self.insert, methods=["POST"])
+        self.bp.add_url_rule(f"{self.prefix}edit/<int:id>", view_func=self.edit)
+        self.bp.add_url_rule(f"{self.prefix}update/", view_func=self.update, methods=["PUT"])
+        self.bp.add_url_rule(f"{self.prefix}delete/<int:id>", view_func=self.delete, methods=["DELETE"])
 
     def get_all(self):
         lessons = Lesson.query.all()
         return render_template("admin/lessons/index.html", lessons=lessons)
 
-    def get_by_id(self, id):
+    def get(self, id):
         lesson = Lesson.query.get(id)
         return render_template("admin/lessons/view.html", lesson=lesson)
 
     def create(self):
-        if request.method == "GET":
-            return render_template("admin/lessons/create.html")
-        else:
-            title = request.form["title"]
-            category = request.form["category"]
-            level = request.form["level"]
+        return render_template("admin/lessons/create.html")
 
-            new_lesson = Lesson(title=title, category=category, level=level)
-            db.session.add(new_lesson)
-            db.session.commit()
-            return redirect("/")
+    def insert(self):
+        title = request.form["title"]
+        category = request.form["category"]
+        level = request.form["level"]
+
+        new_lesson = Lesson(title=title, category=category, level=level)
+        db.session.add(new_lesson)
+        db.session.commit()
+        return redirect("/")
 
     def edit(self, id):
         lesson = Lesson.query.get(id)
-        if request.method == "GET":
-            return render_template("admin/lessons/edit.html", lesson=lesson)
-        elif request.method == "PUT":
+        return render_template("admin/lessons/edit.html", lesson=lesson)
+
+    def update(self):
+        if request.method == "PUT":
             data = request.json
+            id = data.get("id")
+            lesson = Lesson.query.get(id)
             lesson.title = data.get("title")
             lesson.category = data.get("category")
             lesson.level = data.get("level")
             db.session.commit()
+            print(data)
             return jsonify({"success": True}), 204
 
-
-    def delete(self):
-        pass
+    def delete(self, id):
+        if request.method == "DELETE":
+            lesson = Lesson.query.get(id)
+            db.session.delete(lesson)
+            db.session.commit()
+            return jsonify({"success": True}), 204
